33044046ccc70cdfb6acfdc29a6af73d483ff0c2
sellenbogen <sellenbogen@gms.ca>
2021-01-11 17:44:05 -0500

    fixed error page and added it to build phase of upload.sh part 3 (dear lord)

--- upload.sh
+++ upload.sh
@@ -3,6 +3,6 @@
 cd WakeWebsite
 npm install
 ng build
-cp src/error.html WakeWebsite/dist/WakeWebsite/error.html
+cp src/error.html dist/WakeWebsite/error.html
 aws s3 sync dist/WakeWebsite/ s3://wakemirror --delete
 cd ..
