2a6aad0453901f2b1336a90af76abe0af321e9e5
Sholom Ellenbogen <sdellenb@uwaterloo.ca>
2021-01-13 14:34:44 -0500

    Routing test (#2)
    * changed routing so null path is same as home without redirect
    
    * changed routing so null path is same as home without redirect
    
    * changed navbar to its own component, and added error component
    
    * removed routing-test branch from workflow
    
    Co-authored-by: sellenbogen <sellenbogen@gms.ca>

--- WakeWebsite/src/app/app-routing.module.ts
+++ WakeWebsite/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {AboutUsComponent} from './about-us/about-us.component';
 import {LogbookComponent} from './logbook/logbook.component';
+import {ErrorPageComponent} from './error-page/error-page.component';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: '/home'},
-  {path: 'home', component: HomeComponent},
-  {path: 'about-us', component: AboutUsComponent},
-  {path: 'logbook', component: LogbookComponent}
+    {path: '', pathMatch: 'full', redirectTo: '/home'},
+    {path: 'home', component: HomeComponent},
+    {path: 'about-us', component: AboutUsComponent},
+    {path: 'logbook', component: LogbookComponent},
+    {path: '**', pathMatch: 'full', component: ErrorPageComponent}
 ];
 
 @NgModule({
--- WakeWebsite/src/app/app.module.ts
+++ WakeWebsite/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { LogbookComponent } from './logbook/logbook.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { NavbarCustomComponent } from './navbar-custom/navbar-custom.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutUsComponent,
-    LogbookComponent
+    LogbookComponent,
+    ErrorPageComponent,
+    NavbarCustomComponent
   ],
   imports: [
     BrowserModule,
