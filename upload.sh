bf3ff23d40e71212e87ba20343fedbab07e2da6f
DomFaryna <dominic.faryna@gmail.com>
2021-01-22 18:51:47 -0500

    This should not work in actions

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This is a temp upload. Will need to update later to either the offical bucket or wherever UW tells us to upload
+set -e
 cd WakeWebsite
-aws s3 sync dist/WakeWebsite/ s3://${WEBSITE} --delete
+aws s3 sync dist/WakeWebsite/ s3://pi --delete
 cd ..
