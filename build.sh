7f06991314da808276d908a6096b329e1f7c6135
DomFaryna <dominic.faryna@gmail.com>
2021-01-22 18:54:34 -0500

    Added erroring out on build and upload scripts

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 cd WakeWebsite
 npm install
 ng build
--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 cd WakeWebsite
-aws s3 sync dist/WakeWebsite/ s3://pi --delete
+aws s3 sync dist/WakeWebsite/ s3://${WEBSITE} --delete
 cd ..
