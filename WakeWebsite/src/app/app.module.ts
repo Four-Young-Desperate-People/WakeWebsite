8876d4b4cadeab055fe9825b688f8c2db088a639
Sholom Ellenbogen <sdellenb@uwaterloo.ca>
2021-01-22 20:27:59 -0500

    Dayton images + moar (#8)
    * Added test resolutions
    
    * Rest of the images, minus CAD. New Sholom picture
    
    * added new images to homepage carosell, and added all but Dom's "about" sections
    
    * Everything is good now
    
    * finished first draft of homepage, added second blog template
    
    Co-authored-by: DomFaryna <dominic.faryna@gmail.com>

--- WakeWebsite/src/app/app.module.ts
+++ WakeWebsite/src/app/app.module.ts
@@ -1,28 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { BlogComponent } from './blog/blog.component';
-import { ErrorPageComponent } from './error-page/error-page.component';
-import { NavbarCustomComponent } from './navbar-custom/navbar-custom.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BlogPostFirstWeekComponent } from './blog-posts/blog-post-first-week/blog-post-first-week.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {AboutUsComponent} from './about-us/about-us.component';
+import {BlogComponent} from './blog/blog.component';
+import {ErrorPageComponent} from './error-page/error-page.component';
+import {NavbarCustomComponent} from './navbar-custom/navbar-custom.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BlogPostOneComponent} from './blog-posts/blog-post-one/blog-post-one.component';
+import {BlogPostTwoComponent} from './blog-posts/blog-post-two/blog-post-two.component';
 import {MatCardModule} from '@angular/material/card';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutUsComponent,
-    BlogComponent,
-    ErrorPageComponent,
-    NavbarCustomComponent,
-    BlogPostFirstWeekComponent
-  ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        AboutUsComponent,
+        BlogComponent,
+        ErrorPageComponent,
+        NavbarCustomComponent,
+        BlogPostOneComponent,
+        BlogPostTwoComponent
+    ],
     imports: [
         BrowserModule,
         AppRoutingModule,
@@ -30,7 +32,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
         MatCardModule,
         FlexLayoutModule
     ],
-  providers: [],
-  bootstrap: [AppComponent]
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
